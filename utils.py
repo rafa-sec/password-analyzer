21f201f9703ee3bcac6f3d43cc910c537bd0b3c3
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-08 14:40:34 -0300

    Added character diversity function to utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ def get_password():
 
 def get_password_length(password):
     return len(password)
+
+def get_character_diversity(s):
+    return {
+        "number": any(c.isdigit() for c in s),
+        "character": any(c.isalpha() for c in s),
+        "symbol": any(not c.isalnum() for c in s),
+        "uppercase": any(c.isupper() for c in s),
+        "lowercase": any(c.islower() for c in s),
+    }
