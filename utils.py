0acf09a4a05028a617d3802711f86dacf5f25fcb
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-13 16:35:34 -0300

    Entropy estimation feature added to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+points = 0
+
 def get_password():
     return input("Insert your password here: ")
 
@@ -11,4 +13,27 @@ def get_character_diversity(s):
         "symbol": any(not c.isalnum() for c in s),
         "uppercase": any(c.isupper() for c in s),
         "lowercase": any(c.islower() for c in s),
-    }
+}
+
+def set_password_points(password_length, password_diversity, points):
+    if password_length < 8 :
+        points -= 1
+    elif password_length <= 12:
+       points += 0 
+    elif password_length <= 14:
+        points += 2
+    elif password_length > 14:
+        points += 3
+        
+    if password_diversity["number"]:
+       points += 1
+    if password_diversity["character"]:
+       points += 1
+    if password_diversity["symbol"]:
+       points += 1
+    if password_diversity["uppercase"]:
+        points += 1
+    if password_diversity["lowercase"]:
+        points += 1
+
+    return points
