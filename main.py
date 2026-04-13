e1b7db89ca2155856ab9f28e853c8a8e3ad0ec40
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-13 16:35:00 -0300

    Entropy estimation feature added to main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from utils import get_password, get_password_length
-from analyzer import set_password_status
+from utils import get_password, get_password_length, get_character_diversity,set_password_points, points
+from analyzer import  set_password_status
+
+
+#Base variables
 
 password = get_password()
 password_length = get_password_length(password)
 password_diversity = get_character_diversity(password)
+points = set_password_points(password_length, password_diversity, points)
+password_status = set_password_status(points)
 
-status = set_password_status(password_length)
 
 print(f"""
 Your password: {password}
 Length: {password_length}
-Strength: {status}
+Strength: {password_status}
+Password points: {points}
 
 Character diversity: {password_diversity}
 """)
+
