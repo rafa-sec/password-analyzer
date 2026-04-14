3752425d429af10ae3a30ae1d6faff80980c7117
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-14 15:56:21 -0300

    New security rules added to utils.py

--- utils.py
+++ utils.py
@@ -15,25 +15,38 @@ def get_character_diversity(s):
         "lowercase": any(c.islower() for c in s),
 }
 
-def set_password_points(password_length, password_diversity, points):
+def has_too_many_repeats(password, max_repeats=4):
+    count = 1
+
+    for i in range(1, len(password)):
+        if password[i] == password[i - 1]:
+            count += 1
+            if count > max_repeats:
+                return True
+        else:
+            count = 1
+
+    return False
+
+def set_password_points(password_length, password_diversity, points, password):
     if password_length < 8 :
         points -= 1
     elif password_length <= 12:
-       points += 0 
+       points += 1
     elif password_length <= 14:
         points += 2
     elif password_length > 14:
         points += 3
         
-    if password_diversity["number"]:
-       points += 1
-    if password_diversity["character"]:
-       points += 1
+    if password_diversity["number"] and password_diversity["character"]:
+        points += 1
     if password_diversity["symbol"]:
-       points += 1
-    if password_diversity["uppercase"]:
         points += 1
-    if password_diversity["lowercase"]:
+    if password_diversity["uppercase"] and password_diversity["lowercase"]:
         points += 1
+
+
+    if has_too_many_repeats(password):
+        points -= 3
 
     return points
